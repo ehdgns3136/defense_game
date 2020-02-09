3e5b359024a89d7dae2132a18ebbeb7f8c9e3dcd
ehdgns3136 <ehdgns3136@gmail.com>
2020-02-09 23:23:14 +0900

    initialize defensegame unity project

---
+++ defensegame/Assets/Script/Cell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Cell : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Transform child;
+    
+    private void OnEnable()
+    {
+        child = transform.GetChild(0);
+        Debug.Log("fuck you");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        child.GetComponent<Image>().color = Color.gray;
+        Debug.Log("fuck you3");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        child.GetComponent<Image>().color = Color.white;
+        Debug.Log("fuck you4");
+    }
+}
